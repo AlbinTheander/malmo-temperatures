cfc7b21f965e9b960f6e674008ed688388c4d5b1
Albin Theander <Albin.Theander@jayway.com>
2013-01-10 16:18:58 +0100

    Updated temperatures to full scope

--- web.js
+++ web.js
@@ -5,12 +5,24 @@ var data = require('./data/temperatures');
 var app = express.createServer(express.logger());
 
 app.get('/temp', function(request, response) {
-  var from = request.query.from;
-  var to = request.query.to;
-  response.send(new Date(from));
+  var from = new Date(request.query.from);
+  var to = new Date(request.query.to);
+  response.send(getTemps(from, to));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function getTemps(from, to) {
+  var result = new Array();
+  var fromStamp = from.getTime();
+  var toStamp = to.getTime();
+  for(var i = 0; i < data.length; i++) {
+    var entry = data[i];
+    if (entry.timestamp >= toStamp) break;
+    if (entry.timestamp >= fromStamp) result.push(entry);
+  }
+  return result;
+}
